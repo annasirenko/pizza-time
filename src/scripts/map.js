0ca700b7cca56558085c0305c789cd240f9fbd31
Anna <otherjam@gmail.com>
2021-02-16 17:28:02 +0200

    added google maps

---
+++ src/scripts/map.js
@@ -0,0 +1,119 @@
+let map;
+
+function initMap() {
+    let pos = {lat: 48.46368067862552, lng: 35.05193892890304}
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: pos,
+        zoom: 17,
+        styles: [
+            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+            {
+              featureType: "administrative.locality",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "poi",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "geometry",
+              stylers: [{ color: "#263c3f" }],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#6b9a76" }],
+            },
+            {
+              featureType: "road",
+              elementType: "geometry",
+              stylers: [{ color: "#38414e" }],
+            },
+            {
+              featureType: "road",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#212a37" }],
+            },
+            {
+              featureType: "road",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#9ca5b3" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry",
+              stylers: [{ color: "#746855" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#1f2835" }],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#f3d19c" }],
+            },
+            {
+              featureType: "transit",
+              elementType: "geometry",
+              stylers: [{ color: "#2f3948" }],
+            },
+            {
+              featureType: "transit.station",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#d59563" }],
+            },
+            {
+              featureType: "water",
+              elementType: "geometry",
+              stylers: [{ color: "#17263c" }],
+            },
+            {
+              featureType: "water",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#515c6d" }],
+            },
+            {
+              featureType: "water",
+              elementType: "labels.text.stroke",
+              stylers: [{ color: "#17263c" }],
+            },
+          ],
+    
+    });
+
+    // const svgMarker = {
+    //     path:
+    //       "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+    //     fillColor: "blue",
+    //     fillOpacity: 0.6,
+    //     strokeWeight: 0,
+    //     rotation: 0,
+    //     scale: 2,
+    //     anchor: new google.maps.Point(20, 15),
+    //   };
+
+    let marker = new google.maps.Marker({
+        map: map,
+        position: pos,
+        title: "Мы здесь!",
+        // icon: "./img/icons/pizza-slice.svg",
+        // icon: svgMarker,
+    });
+
+    let info = new google.maps.InfoWindow({
+        content: '<h3>Твоя пицца здесь</h3><p>ул. Гопнер,4а</p>'
+    });
+
+    marker.addEventListener("click", function() {
+        info.open(map, marker);
+    });    
+   
+
+}
